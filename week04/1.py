5371cd794fde34c7d1577986ba3e6a38bbd8ffa7
artemy <pytuikov2002@yandex.ru>
2020-09-26 10:44:36 +0300

    1

--- week04/1.py
+++ week04/1.py
@@ -1,9 +1,7 @@
 import numpy as np
 i=0
-x=1
+a=[1,100,1000]
 y=[]
-while i<3:
-    x=x*10
+for x in a:
     y.append(float(np.log(np.exp(1/(np.sin(x)+1))/(5/4+x**15))/(1+x**2)))
-    i=i+1
 print(y)
