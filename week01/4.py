d204640675884766ac581ca21a41f9b7ec469395
artemy <pytuikov2002@yandex.ru>
2020-10-03 10:05:55 +0300

    1

---
+++ week01/4.py
@@ -0,0 +1,16 @@
+import turtle
+for i in range(10):
+    turtle.left(180)
+    turtle.forward(10+10*i)
+    turtle.left(90)
+    turtle.forward(10+10*i)
+    turtle.left(90)
+    turtle.forward(10+10*i)
+    turtle.left(90)
+    turtle.forward(10+10*i)
+    turtle.penup()
+    turtle.forward(5)
+    turtle.right(90)
+    turtle.forward(5)
+    turtle.pendown()
+
